52e358ea137c43e577791d39b72094f841f6fd49
Alex Flores Escarcega <alex.floresescar@gmail.com>
2016-10-30 02:21:16 -0500

    Add finished proj

--- TempBeeper.ino
+++ TempBeeper.ino
@@ -8,35 +8,30 @@
 #define ECHO_PIN     11
 #define MAX_DISTANCE 200
 #define inPin 10 // for temp sensor
-#define speakerPin =  8;      //Buzzer pin;
+#define speakerPin 8      //Buzzer pin;
 
 NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 OneWire ds(inPin);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  pinMode(speakerPin, OUTPUT);
-}
 
-void loop() {
-  // put your main code here, to run repeatedly:
-//  printDistance();
-  printTemp();
-  
-}
 
-void printDistance(){
+double getDistanceInCM(){
   delay(50);
-  int uS = sonar.ping();
+  int uS = sonar.ping() / US_ROUNDTRIP_CM;
+  return(uS);
+}
+void printDistance(){
+//  delay(50);
+//  int uS = sonar.ping();
   Serial.print("Ping: ");
-  Serial.print(uS / US_ROUNDTRIP_CM);
+//  Serial.print(uS / US_ROUNDTRIP_CM);
+  Serial.print(getDistanceInCM());
   Serial.println("cm");
 }
 
-void printTemp(){
-  int HighByte, LowByte, TReading, SignBit, Tc_100, Whole, Fract;
+int returnTc_100(){
+    int HighByte, LowByte, TReading, SignBit, Tc_100, Whole, Fract;
   byte i;
   byte present = 0;
   byte data[12];
@@ -44,7 +39,7 @@ void printTemp(){
  
   if ( !ds.search(addr)) {
       ds.reset_search();
-      return;
+      return 0;
   }
  
  
@@ -64,7 +59,6 @@ void printTemp(){
   for ( i = 0; i < 9; i++) { 
     data[i] = ds.read();
   }
-  Serial.print("Temperature: ");
   LowByte = data[0];
   HighByte = data[1];
   TReading = (HighByte << 8) + LowByte;
@@ -74,12 +68,24 @@ void printTemp(){
     TReading = (TReading ^ 0xffff) + 1;
   }
   Tc_100 = (6 * TReading) + TReading / 4; 
-  Whole = Tc_100 / 100; 
-  Fract = Tc_100 % 100;
-  if (SignBit)
-  {
-     Serial.print("-");
-  }
+  return Tc_100;
+}
+
+double getCelsius(int Tc_100){
+  return Tc_100/100 + Tc_100%100/100.0;
+  
+}
+
+void printTemp(){
+
+//  if (SignBit)
+//  {
+//     Serial.print("-");
+//  }
+  int Tc_100 = returnTc_100();
+  int Whole = Tc_100 / 100; 
+  int Fract = Tc_100 % 100;
+  Serial.print("Temperature: ");
   Serial.print(Whole);
   Serial.print(".");
   if (Fract < 10)
@@ -90,3 +96,36 @@ void printTemp(){
   Serial.print(" C\n");
 }
 
+void makeSiren(){
+    digitalWrite(speakerPin,LOW);
+    digitalWrite(speakerPin, HIGH);
+    delay(1000);
+    digitalWrite(speakerPin,LOW);
+    digitalWrite(speakerPin, HIGH);
+    delay(.75);
+    digitalWrite(speakerPin,LOW);
+    digitalWrite(speakerPin, HIGH);
+    delay(.75);
+    digitalWrite(speakerPin,LOW);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(speakerPin, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  printDistance();
+  printTemp();
+//  Serial.print("this is the temp: "); 
+//  Serial.print(getCelsius(returnTc_100()));
+//  Serial.print("\n");
+  int distance = getDistanceInCM();
+  if( distance > 30 && distance < 60 && getCelsius(returnTc_100()) > 25){
+    Serial.println("Yes");
+    makeSiren();
+  }
+  
+}
